4540bc024c64ab271c3dc211f62b3aaa42790e53
Concurrent <C.Y.Concurrent@gmail.com>
2019-08-29 21:46:07 +0800

    js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,13 +9,10 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-
-@Component({
-  components: {}
-})
-export default class Home extends Vue {}
+<script>
+export default {
+  
+}
 </script>
 
 <style lang="scss" scoped>
